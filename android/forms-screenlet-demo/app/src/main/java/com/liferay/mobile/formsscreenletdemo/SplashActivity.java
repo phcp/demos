af67776bac75820097b095539df20d2382f63287
Paulo Cruz <paulo.cruz@liferay.com>
2018-10-22 18:39:12 -0300

    Create splash activity

---
+++ android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.liferay.mobile.formsscreenletdemo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.liferay.mobile.screens.context.LiferayScreensContext;
+import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.context.storage.CredentialsStorageBuilder;
+import com.liferay.mobile.formsscreenletdemo.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(() -> {
+
+			LiferayScreensContext.init(this);
+			SessionContext.loadStoredCredentialsAndServer(CredentialsStorageBuilder.StorageType.SHARED_PREFERENCES);
+
+			//if (SessionContext.hasUserInfo()) {
+			//	startActivity(HomeActivity.class);
+			//} else {
+			//	startActivity(LoginActivity.class);
+			//}
+
+			finish();
+
+		}, 2000);
+	}
+
+	private void startActivity(Class<?> clazz) {
+		Intent intent = new Intent(SplashActivity.this, clazz);
+		startActivity(intent);
+	}
+}
