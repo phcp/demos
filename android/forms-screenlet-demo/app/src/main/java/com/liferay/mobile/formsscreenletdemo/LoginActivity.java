b824fff38950a82a29e185c5bdd4443da69e72c9
Paulo Cruz <paulo.cruz@liferay.com>
2018-10-22 18:40:41 -0300

    Create login, sign up and forgot password activities

---
+++ android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.liferay.mobile.formsscreenletdemo;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.liferay.mobile.screens.auth.login.LoginListener;
+import com.liferay.mobile.screens.auth.login.LoginScreenlet;
+import com.liferay.mobile.screens.context.User;
+import com.liferay.mobile.screens.util.AndroidUtil;
+import com.liferay.mobile.formsscreenletdemo.R;
+import com.liferay.mobile.formsscreenletdemo.util.FormsUtil;
+
+/**
+ * @author Luísa Lima
+ */
+public class LoginActivity extends AppCompatActivity implements LoginListener {
+
+	private CoordinatorLayout coordinatorLayout;
+	private LoginScreenlet loginScreenlet;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		coordinatorLayout = findViewById(R.id.coordinator);
+		loginScreenlet = findViewById(R.id.login);
+		loginScreenlet.setListener(this);
+
+		FormsUtil.setLightStatusBar(this, getWindow());
+
+		TextView forgotPasswordText = findViewById(R.id.liferay_forgot_link);
+		forgotPasswordText.setOnClickListener(this::startForgotPasswordActivity);
+
+		Button signUpText = findViewById(R.id.liferay_signup_button);
+		signUpText.setOnClickListener(this::startSignUpActivity);
+
+		setDefaultValues();
+	}
+
+	private void startSignUpActivity(View view) {
+		Intent intent = new Intent(this, SignUpActivity.class);
+		startActivity(intent);
+	}
+
+	private void startForgotPasswordActivity(View view) {
+		Intent intent = new Intent(this, ForgotPasswordActivity.class);
+		startActivity(intent);
+	}
+
+	private void startHomeActivity() {
+		//Intent intent = new Intent(this, HomeActivity.class);
+		//finish();
+		//startActivity(intent);
+	}
+
+	@Override
+	public void onLoginSuccess(User user) {
+		startHomeActivity();
+	}
+
+	@Override
+	public void onLoginFailure(Exception e) {
+		int icon = R.drawable.default_error_icon;
+		int backgroundColor =
+			ContextCompat.getColor(this, com.liferay.mobile.screens.viewsets.lexicon.R.color.lightRed);
+		int textColor = Color.WHITE;
+		String message = getString(R.string.login_failed);
+
+		AndroidUtil.showCustomSnackbar(coordinatorLayout, message, Snackbar.LENGTH_LONG, backgroundColor, textColor,
+			icon);
+	}
+
+	@Override
+	public void onAuthenticationBrowserShown() {
+
+	}
+
+	private void setDefaultValues() {
+		EditText login = loginScreenlet.findViewById(R.id.liferay_login);
+		login.setText(getString(R.string.default_email));
+
+		EditText password = loginScreenlet.findViewById(R.id.liferay_password);
+		password.setText(getString(R.string.default_password));
+	}
+}
