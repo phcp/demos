f990916ed058cb5739300b9940e0dc2fc9d3abe5
Marcelo Mello <marcelo.mello@liferay.com>
2018-11-19 15:23:42 -0300

    LPS-87442 Create SpecialOffers page

--- android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/view/sessions/SpecialOffersActivity.java
+++ android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/view/sessions/SpecialOffersActivity.java
@@ -1,55 +1,53 @@
 package com.liferay.mobile.formsscreenletdemo.view.sessions;
 
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import com.liferay.mobile.screens.util.AndroidUtil;
-import com.liferay.mobile.screens.webcontent.WebContent;
-import com.liferay.mobile.screens.webcontent.display.WebContentDisplayListener;
-import com.liferay.mobile.screens.webcontent.display.WebContentDisplayScreenlet;
+
 import com.liferay.mobile.formsscreenletdemo.R;
+import com.liferay.mobile.screens.util.AndroidUtil;
+import com.liferay.mobile.screens.web.WebListener;
+import com.liferay.mobile.screens.web.WebScreenlet;
+import com.liferay.mobile.screens.web.WebScreenletConfiguration;
 
-public class SpecialOffersActivity extends AppCompatActivity implements WebContentDisplayListener {
+public class SpecialOffersActivity extends AppCompatActivity implements WebListener {
 
-	WebContentDisplayScreenlet webContentDisplayScreenlet;
+    WebScreenlet webScreenlet;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_special_offers);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_special_offers);
 
-		webContentDisplayScreenlet = findViewById(R.id.web_content_display_screenlet);
-		webContentDisplayScreenlet.setCustomCssFile(R.raw.special_offers_custom);
-		webContentDisplayScreenlet.setListener(this);
-		webContentDisplayScreenlet.load();
-	}
+        webScreenlet = findViewById(R.id.web_screenlet);
+        webScreenlet.setListener(this);
+        webScreenlet.setWebScreenletConfiguration(
+            new WebScreenletConfiguration.Builder("/web/guest/special-offers")
+                .addRawCss(R.raw.special_offers_custom, "special_offers_custom.css")
+                .load()
+        );
+        webScreenlet.load();
+    }
 
-	@Override
-	public void error(Exception e, String userAction) {
-		int icon = R.drawable.default_error_icon;
-		int backgroundColor =
-			ContextCompat.getColor(this, com.liferay.mobile.screens.viewsets.lexicon.R.color.lightRed);
+    @Override
+    public void error(Exception e, String userAction) {
+        int icon = R.drawable.default_error_icon;
+        int backgroundColor =
+            ContextCompat.getColor(this, com.liferay.mobile.screens.viewsets.lexicon.R.color.lightRed);
 
-		AndroidUtil.showCustomSnackbar(webContentDisplayScreenlet, userAction, Snackbar.LENGTH_LONG, backgroundColor,
-			Color.WHITE, icon);
-	}
+        AndroidUtil.showCustomSnackbar(webScreenlet, userAction, Snackbar.LENGTH_LONG, backgroundColor,
+            Color.WHITE, icon);
+    }
 
-	@Override
-	public WebContent onWebContentReceived(WebContent html) {
-		return null;
-	}
+    @Override
+    public void onPageLoaded(String url) {
 
-	@Override
-	public boolean onUrlClicked(String url) {
-		return false;
-	}
+    }
 
-	@Override
-	public boolean onWebContentTouched(View view, MotionEvent event) {
-		return false;
-	}
+    @Override
+    public void onScriptMessageHandler(String namespace, String body) {
+
+    }
 }
