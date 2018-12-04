74791ea1d924478f87b3d06da74bdf1b8c031763
camisLuisa <clfl@ecomp.poli.br>
2018-12-04 17:48:44 -0300

    MOBILE-1005 SF

--- android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/util/DemoUtil.java
+++ android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/util/DemoUtil.java
@@ -47,7 +47,6 @@ public class DemoUtil {
 		window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 	}
 
-	public static void showNegativeSnackBar(Context context, View layout) {
 	public static void showErrorSnackBar(Context context, View layout) {
 		int icon = R.drawable.default_error_icon;
 		int backgroundColor =
