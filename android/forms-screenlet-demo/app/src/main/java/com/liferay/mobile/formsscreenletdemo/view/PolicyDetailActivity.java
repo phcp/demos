9979c0a3d8b935e99957e72ba85819e818eefbf0
camisLuisa <clfl@ecomp.poli.br>
2018-12-10 11:34:00 -0300

    MOBILE-1008 Create Policy Download Action

--- android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/view/PolicyDetailActivity.java
+++ android/forms-screenlet-demo/app/src/main/java/com/liferay/mobile/formsscreenletdemo/view/PolicyDetailActivity.java
@@ -1,9 +1,20 @@
 package com.liferay.mobile.formsscreenletdemo.view;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.liferay.mobile.formsscreenletdemo.R;
 import com.liferay.mobile.formsscreenletdemo.util.Constants;
 import com.liferay.mobile.formsscreenletdemo.util.DemoUtil;
@@ -12,6 +23,9 @@ import com.liferay.mobile.screens.asset.display.AssetDisplayInnerScreenletListen
 import com.liferay.mobile.screens.asset.display.AssetDisplayListener;
 import com.liferay.mobile.screens.asset.display.AssetDisplayScreenlet;
 import com.liferay.mobile.screens.base.BaseScreenlet;
+import javax.security.auth.callback.Callback;
+
+import static com.liferay.mobile.screens.context.LiferayScreensContext.getContext;
 
 /**
  * @author Vitória Mendes
@@ -21,6 +35,9 @@ public class PolicyDetailActivity extends AppCompatActivity implements AssetDisp
 
     private AssetDisplayScreenlet assetDisplayScreenlet;
     private Toolbar toolbar;
+    private DownloadManager downloadManager;
+    private String assetEntryUrl;
+    private String assetEntryTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +50,55 @@ public class PolicyDetailActivity extends AppCompatActivity implements AssetDisp
         assetDisplayScreenlet = findViewById(R.id.my_policy);
 
         long entryId = getIntent().getLongExtra(Constants.ENTRY_ID_KEY, 0);
+        assetEntryUrl = getIntent().getStringExtra(Constants.ASSET_ENTRY_URL);
+        assetEntryTitle = getIntent().getStringExtra(Constants.ASSET_ENTRY_TITLE);
 
         assetDisplayScreenlet.setEntryId(entryId);
         assetDisplayScreenlet.loadAsset();
         assetDisplayScreenlet.setListener(this);
         assetDisplayScreenlet.setConfigurationListener(this);
+
+        downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.policy_detail_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.download_policy) {
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 112);
+            } else {
+                String url = getResources().getString(R.string.liferay_server) + assetEntryUrl;
+                new DownloadPolicyService().downloadPolicy(url, assetEntryTitle, downloadManager,
+                    () -> runOnUiThread(() -> showDownloadCompletedToast()));
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void showDownloadCompletedToast() {
+        Toast toast = new Toast(getContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        String message = getResources().getString(R.string.download_file) + " " + assetEntryTitle + " " + getResources().getString(R.string.completed);
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View layout = inflater.inflate(R.layout.toast_layout_default, findViewById(R.id.toast_layout_default));
+        TextView ratingToastMessage = layout.findViewById(R.id.toast_message);
+        ratingToastMessage.setText(message);
+        toast.setView(layout);
+        toast.show();
+
     }
 
     @Override
